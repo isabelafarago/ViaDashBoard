16bddb3299a9c003e0e715bb8e2e7eaa927db444
Isabela Ferreira Farago <109293295+isabelafarago@users.noreply.github.com>
2023-05-26 17:31:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 //-------------------- Charts responsible to catch the data from all employees sales --------------------
-fetch('data.json')
+fetch('data/data.json')
   .then(response => response.json())
   .then(data => {
     const employeeSales = data.map(employee => employee.liquido);
     const employeeNames = data.map(employee => employee.nome);
-    const filteredEmployeeSales = []; const filteredEmployeeNames = [];
+    const filteredEmployeeSales = [];
+    const filteredEmployeeNames = [];
 
     for (let i = 0; i < employeeSales.length; i++) {
       filteredEmployeeSales.push(employeeSales[i]);
-         filteredEmployeeNames.push(employeeNames[i]);
+      filteredEmployeeNames.push(employeeNames[i]);
     }
-    // Calculate the minimum sales value
     const minSales = Math.min(...employeeSales);
     const ctx = document.getElementById('employeeChart').getContext('2d');
-    
+
     const chart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: filteredEmployeeNames, 
+        labels: filteredEmployeeNames,
         datasets: [{
-          label: 'Employee Sales',
-          data: filteredEmployeeSales, 
-          backgroundColor: '#2a2fc7',
-          borderWidth: 1,
+          label: 'Vendas: ',
+          data: filteredEmployeeSales,
+          backgroundColor: [
+            '#353CD5',
+            '#C389F3',
+            '#D86375',
+            '#6BC5BB',
+            '#F1A861',
+            '#D845375',
+          ],
+          borderWidth: 0,
+          borderColor: 'black',
           borderRadius: 25
         }]
       },
@@ -31,29 +39,53 @@ fetch('data.json')
         scales: {
           y: {
             beginAtZero: true,
-            min: 0, 
+            min: 0,
             ticks: {
-              stepSize: 10000,
+              stepSize: 1, 
               color: '#ffff'
-            },
+            }
           },
           x: {
             ticks: {
-              stepSize: 0,
-              color: '#ffff'
+              min: 0,
+              stepSize: 1,
+              color: '#fff'
             },
             beginAtZero: true
+          }
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: 'top',
+            labels: {
+              color: "white",
+              font: {
+                size: 14
+              }
+            }
+          },
+          title: {
+            display: true,
+            text: 'Ranking dos funcionários',
+            align: "start",
+            color: 'white',
+            font: {
+              size: 16,
+              family: 'Arial',
+              weight: 'bold'
+            }
           }
         }
       }
     });
+
   })
   .catch(error => console.error(error));
 
 
-
 //-------------------- PieCharts responsible to catch the data from all employees sales --------------------
-fetch('data2.json')
+fetch('data/data2.json')
   .then(response => response.json())
   .then(data => {
     const paymentMethodTotals = {};
@@ -70,53 +102,80 @@ fetch('data2.json')
     const paymentMethodNames = Object.keys(paymentMethodTotals);
     const paymentMethodTotalsData = Object.values(paymentMethodTotals);
     const ctx = document.getElementById('paymentMethodChart').getContext('2d');
-  
+
     const chart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-          labels: paymentMethodNames,
-          datasets: [{
-            label: 'Employee Sales by Payment Method',
-            data: paymentMethodTotalsData,
-            backgroundColor: [
-              '#653CD5',
-              '#C389F3',
-              '#D86375',
-              '#6BC5BB',
-              '#F1A861',
-              '#db3f9a'
-            ],
-            borderColor: [
-              '#653CD5',
-              '#C389F3',
-              '#D86375',
-              '#6BC5BB',
-              '#F1A861',
-              '#db3f9a'
-            ],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          responsive: true,
-          scales: {
-            y: {
-              ticks: {
-                color: '#2E3442'
-              },
-              beginAtZero: true
+      type: 'pie',
+      data: {
+        labels: paymentMethodNames,
+        datasets: [{
+          label: "",
+          data: paymentMethodTotalsData,
+          backgroundColor: [
+            '#653CD5',
+            '#C389F3',
+            '#D86375',
+            '#6BC5BB',
+            '#F1A861',
+            '#db3f9a'
+          ],
+          borderColor: [
+            '#653CD5',
+            '#C389F3',
+            '#D86375',
+            '#6BC5BB',
+            '#F1A861',
+            '#db3f9a'
+          ],
+          borderWidth: 0
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            ticks: {
+              color: 'white'
             },
-            x: {
-              ticks: {
-                color: '#2E3442'
-              },
-              beginAtZero: true
+            beginAtZero: true
+          },
+          x: {
+            ticks: {
+              color: 'white'
+            },
+            beginAtZero: true
+          }
+        },
+          plugins: {
+            legend: {
+              display: true,
+              position: 'top',
+              labels: {
+                color: "white",
+                font: {
+                  size: 11
+                }
+              }
+            },
+          title: {
+            display: true,
+            text: 'Formas de pagamento mais usadas',
+            align: 'start',
+            color: 'white', 
+            padding: {
+              top: 10,
+              bottom: 10
+            },
+            font: {
+              size: 16,
+              weight: 'bold'
             }
           }
         }
-      });
-    })
-    .catch(error => console.error(error));
+      }
+    });
+  })
+  .catch(error => console.error(error));
+
   
   //-------------------- employees list in the other page --------------------
  document.addEventListener("DOMContentLoaded", function() {
@@ -124,15 +183,13 @@ fetch('data2.json')
     const backButton = document.getElementById("backButton");
     
     function showEmployeeDetails(employeeId) {
-      fetch("data.json")
+      fetch("data/data.json")
         .then(response => response.json())
         .then(data => {
           const employeeDetails = data.find(detail => detail.id == employeeId);
-          
-          // HTML for the employee details no the emplooye page
           const employeeDetailsHtml =
             `<div class="card">
-              <h2>ID: ${employeeDetails.id} ${employeeDetails.nome}</h2>
+              <h2> ${employeeDetails.nome} (ID: ${employeeDetails.id})</h2>
               <p>BRUTO   - R$${employeeDetails.bruto}</p>
               <p>LÍQUIDO - R$${employeeDetails.liquido}</p>
               <img src="${employeeDetails.image_url}" alt="Employee Image">
@@ -143,7 +200,7 @@ fetch('data2.json')
     }
     // to show the employee list
     function showEmployeeList() {
-      fetch("data.json")
+      fetch("data/data.json")
         .then(response => response.json())
         .then(data => {
           const employeeList = data.map(employee => `<li class="employee" data-id="${employee.id}">${employee.nome}</li>`).join("");
@@ -154,7 +211,7 @@ fetch('data2.json')
           backButton.style.display = "none";
         });
     }
-    fetch("data.json")
+    fetch("data/data.json")
       .then(response => response.json())
       .then(data => {
         const employeeList = data.map(employee => `<li class="employee" data-id="${employee.id}">${employee.nome}</li>`).join("");
@@ -175,12 +232,13 @@ fetch('data2.json')
   });
 
   //--------------------Sum of sales based on product types --------------------
-fetch("data3.json")
-    .then(function (response) {
+  fetch("data/data3.json")
+  .then(function (response) {
     return response.json();
   })
-   .then(function (jsonData) {
-    const idTotals = {}; const idNames = {};
+  .then(function (jsonData) {
+    const idTotals = {};
+    const idNames = {};
     jsonData.forEach(function (array) {
       array.forEach(function (item) {
         if (idTotals[item.id]) {
@@ -188,7 +246,7 @@ fetch("data3.json")
         } else {
           idTotals[item.id] = item.total;
         }
-        idNames[item.id] = item.nome; 
+        idNames[item.id] = item.nome;
       });
     });
     const sortedIds = Object.keys(idTotals).sort(function (a, b) {
@@ -196,11 +254,11 @@ fetch("data3.json")
     });
     const chartData = {
       labels: sortedIds.map(function (id) {
-        return idNames[id]; 
+        return idNames[id];
       }),
       datasets: [
         {
-          label: "Total Sum",
+          label: "Noo presente mês: ",
           data: sortedIds.map(function (id) {
             return idTotals[id];
           }),
@@ -214,10 +272,10 @@ fetch("data3.json")
             "#FF7F00",
             "#8B008B",
             "#00CED1",
-            "#FF1493"
+            "#FF1493",
           ],
           borderWidth: 1,
-          borderRadius: 25
+          borderRadius: 25,
         },
       ],
     };
@@ -226,6 +284,83 @@ fetch("data3.json")
       type: "bar",
       data: chartData,
       options: {
+        indexAxis: "y", 
+        plugins: {
+          legend: {
+            display: false, 
+          },
+          title: {
+            display: true,
+            text: "Ranking de vendas dos produtos no mês",
+            position: "left",
+            color: 'white',
+            font: {
+              size: 16,
+              family: "Arial",
+              weight: "bold",
+            },
+          },
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+            ticks: {
+              color: "#ffff",
+            },
+          },
+          x: {
+            ticks: {
+              stepSize: 10000,
+              color: "#ffff",
+            },
+          },
+        },
+      },
+    });
+  })
+  .catch(function (error) {
+    console.error("Error reading the JSON file:", error);
+  });
+
+  //----------------------------- line chart based on the sales throughout the year ------------------------------
+fetch('data/data4.json')
+  .then(response => response.json())
+  .then(data => {
+    const products = Object.keys(data);
+    const months = Object.keys(data[products[0]]);
+    const colorScheme = [
+      "#008744",
+      "#001eff",
+      "#FF306A",
+      "#FBCE23",
+      "#FFA700",
+      "#C6E74E",
+      "#FF7F00",
+      "#8B008B",
+      "#00CED1",
+      "#d62d20",
+    ];
+    const datasets = products.map((product, index) => ({
+      label: product,
+      data: months.map(month => data[product][month]),
+      borderColor: colorScheme[index],
+      backgroundColor: colorScheme[index],
+      fill: false,
+    }));
+    const ctx = document.getElementById('salesChart').getContext('2d');
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: months,
+        datasets: datasets
+      },
+      options: {
+        responsive: true,
+        elements: {
+          line: {
+            borderWidth: 2
+          }
+        },
         scales: {
           y: {
             beginAtZero: true,
@@ -240,36 +375,58 @@ fetch("data3.json")
             },
           },
         },
-      },
+          plugins: {
+            legend: {
+              display: true,
+              position: 'top',
+              labels: {
+                color: "white",
+                font: {
+                  size: 11
+                }
+              },
+              boxWidth: 0
+            },
+          title: {
+            display: true,
+            text: 'Nichos mais vendidos durante o ano',
+            align: 'start',
+            position: 'top',
+            color: 'white',
+            font: {
+              size: 16,
+              family: "Arial",
+              weight: "bold",
+            },
+          }
+        }
+      }
     });
   })
-  .catch(function (error) {
-    console.error("Error reading the JSON file:", error);
-  });
+
+//-----------------------make the changing theme static --------------------------------------------------
+  const checkbox = document.getElementById('theme');
+  const body = document.body;
+  function applyTheme() {
+      if (checkbox.checked) {
+         body.classList.add('light-mode');
+        } else {
+         body.classList.remove('light-mode');
+    }
+  }
+    function setThemePreference() {
+        localStorage.setItem('theme', checkbox.checked ? 'light' : 'dark');
+    applyTheme();
+  }
+    function getThemePreference() {
+       const theme = localStorage.getItem('theme');
+        if (theme === 'light') {
+        checkbox.checked = true;
+         } else {
+         checkbox.checked = false;
+    }
+    applyTheme();
+  }
+  checkbox.addEventListener('change', setThemePreference);
+  window.addEventListener('DOMContentLoaded', getThemePreference);
   
-  //----------------------------- line chart based on the sales throughout the year ------------------------------
-  fetch('data4.json')
-      .then(response => response.json())
-      .then(data => {
-        const products = Object.keys(data);
-        const months = Object.keys(data[products[0]]);
-        const datasets = products.map(product => ({
-          label: product,
-          data: months.map(month => data[product][month])
-        }));
-        const ctx = document.getElementById('salesChart').getContext('2d');
-        new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: months,
-            datasets: datasets
-          },
-          options: {
-            responsive: true,
-            title: {
-              display: true,
-              text: 'Product Sales Track'
-            }
-        }
-    });
-});
